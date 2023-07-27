f0e2c6778ff31b9960bbdaaee9290059b9f2c3ac
Abadamasi <abbabadamasi33@gmail.com>
2023-07-26 17:05:11 -0700

    initializer.c

---
+++ initializer.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+/**
+ * initialize_all_program_data - to initialize all the data inputs
+ * @tokens: what user write on the prompt
+ * @cur_cmdfile: token to initialize
+ *
+ * Return: 0 on success
+ */
+void initialize_all_program_data(void);
+{
+	tokens = NULL;
+	cur_cmdfile = NULL;
+}
